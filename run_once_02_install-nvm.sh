c146c4df50bb5bacacfc27c0766520fd174752cd
Karim N. Gorjux <karimone@gmail.com>
2024-02-22 12:12:59 +1100

    Fix nvm script

--- run_once_02_install-nvm.sh
+++ run_once_02_install-nvm.sh
@@ -1,31 +1,25 @@
 #!/bin/sh
 # Script to install nvm
 
-NVM_VERSION="v0.39.7"
-
 os_has() {
   type "$1" > /dev/null 2>&1
 }
 
+nvm_setup() {
+    # setup the last lts and use it as default
+    # not used at the moment
+    nvm install --lts
+    nvm alias default stable
+}
 
-if os_has "nv"; then
+if os_has "nvm"; then
     echo "NVM already installed!"
     exit
-fi
-
-
-# install NVM
-if os_has "curl"; then
-    curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh | bash"
-elif os_has "wget"; then
-    wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh | bash"
 else
-    echo "Missing wget or curl!"
-    exit 1  # error!
+    wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
 fi
 
 
-# setup the last lts and use it as default
-nvm install --lts
-nvm alias default stable
+
+
 
