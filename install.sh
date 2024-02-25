e2c1e4c3753eccb8c021a4c5c7e123bffb5392f1
Karim N. Gorjux <karimone@gmail.com>
2024-02-25 15:41:27 +1100

    update

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ os_has() {
   type "$1" > /dev/null 2>&1
 }
 
+export NODE_OPTIONS="--no-deprecation"
+
 if os_has "bw"; then
-    export BW_SESSION=$(bw login --raw)
+    export BW_SESSION=$(bw login --raw) && sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply karimone
 else
     echo "You need to install BW!"
     echo "Installing nvm"
@@ -13,8 +15,6 @@ else
     source ~/.bashrc
     nvm install stable
     nvm use stable
-    export NODE_OPTIONS="--no-deprecation"
-    npm install -g @bitwarden/cli
+    npm install -g @bitwarden/cli && sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply karimone
 fi
 
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply karimone
