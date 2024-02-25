0e9172a6a9e66bc83ea3e60399ad617f452117d1
Karim N. Gorjux <karimone@gmail.com>
2024-02-25 19:47:50 +1100

    Update install

--- install.sh
+++ install.sh
@@ -5,65 +5,23 @@ os_hasnt() {
   ! type "$1" > /dev/null 2>&1
 }
 
-check_bw(){
 if os_hasnt "bw"; then
     echo "You need to install bitwarden-cli! Read the README"
+    echo "You can install using node or the package manager"
+    echo "If you are on ubuntu run sudo snap install bw"
+    echo "Check the README for other methods"
+    echo "https://github.com/karimone/dotfiles"
     exit
 fi
-}
 
-check_jq() {
-if os_hasnt "jq"; then
-    echo "You need to install jq"
-    echo "sudo apt install jq"
-    echo "brew install jq"
-    exit
+if [ -z "$BW_SESSION"]; then
+    echo "You must run this script with the BW_SESSION set"
+    echo "Check the status with bw status"
+    echo "If is unautenthicated run"
+    echo 'export BW_SESSION=$(bw login EMAIL --raw)'
+    echo "If is unlocked run"
+    echo 'export BW_SESSION=$(bw unlock --raw)'
+    exit 
 fi
-}
-
-
-check_email() {
-    if [ -z "$BW_EMAIL" ]; then
-      echo "Set the email to use with bw"
-      echo 'export BW_EMAIL="my@email.com'
-      exit 1
-    fi
-}
-
-
-set_bw_session() {
-    while [ -z "$BW_SESSION" ]; do
-        bw_status=$(bw status | jq -r '.status' 2>/dev/null)
-
-        if [ "$?" -eq 0 ]; then
-            case "$bw_status" in
-                "unauthenticated")
-                    echo "Logging in..."
-                    export BW_SESSION=$(bw login $BW_EMAIL --raw)
-                    exit 1
-                    ;;
-                "locked")
-                    echo "Vault is locked. Unlocking..."
-                    export BW_SESSION=$(bw unlock --raw)
-                    ;;
-                "unlocked")
-                    echo "Vault is already unlocked."
-                    ;;
-                *)
-                    echo "Unknown status: $bw_status. Exiting."
-                    exit 1
-                    ;;
-            esac
-        else
-            echo "Error checking Bitwarden status. Retrying..."
-            sleep 1
-        fi
-    done
-}
-
-check_bw
-check_jq
-check_email
-set_bw_session
 
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply karimone
