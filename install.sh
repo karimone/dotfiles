a52ff0378a9ea566e29b6055c154812ebc28abd4
Karim N. Gorjux <karimone@gmail.com>
2024-02-26 15:23:11 +1100

    Update script. Fancy

--- install.sh
+++ install.sh
@@ -1,27 +1,72 @@
 #!/bin/sh
-export NODE_OPTIONS="--no-deprecation"
 
 os_hasnt() {
   ! type "$1" > /dev/null 2>&1
 }
 
+print_green_frame() {
+  printf "\e[1;32m+%s+\e[0m\n" "$(printf -- '-%.0s' {1..50})"
+}
+
+print_green() {
+  printf "\e[1;32m%s\e[0m\n" "$1"
+}
+
+print_yellow_frame() {
+  printf "\e[1;33m+%s+\e[0m\n" "$(printf -- '-%.0s' {1..50})"
+}
+
+print_yellow() {
+  printf "\e[1;33m%s\e[0m\n" "$1"
+}
+
+print_red_frame() {
+  printf "\e[1;31m+%s+\e[0m\n" "$(printf -- '-%.0s' {1..50})"
+}
+
+print_red() {
+  printf "\e[1;31m%s\e[0m\n" "$1"
+}
+
 if os_hasnt "bw"; then
-    echo "You need to install bitwarden-cli! Read the README"
-    echo "You can install using node or the package manager"
-    echo "If you are on ubuntu run sudo snap install bw"
-    echo "Check the README for other methods"
-    echo "https://github.com/karimone/dotfiles"
+    print_red_frame
+    print_red "You need to install bitwarden-cli! Read the README"
+    print_yellow "You can install it using Node.js or your package manager"
+    print_yellow "If you are on Ubuntu, run:"
+    print_yellow "  sudo snap install bw"
+    print_yellow "Or use Node.js and install the package directly:"
+    print_yellow "  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
+    print_yellow "Then reload the shell to update the paths and other settings"
+    print_yellow "  nvm install stable && nvm use stable"
+    print_yellow "  npm install -g @bitwarden/cli"
+    print_yellow '  export NODE_OPTIONS="--no-deprecation"'
+    print_yellow "Check the README for more details:"
+    print_yellow "https://github.com/karimone/dotfiles"
+    print_red_frame
     exit
 fi
 
 if [ -z "$BW_SESSION" ]; then
-    echo "You must run this script with the BW_SESSION set"
-    echo "Check the status with bw status"
-    echo "If is unautenthicated run"
-    echo 'export BW_SESSION=$(bw login EMAIL --raw)'
-    echo "If is unlocked run"
-    echo 'export BW_SESSION=$(bw unlock --raw)'
+    print_red_frame
+    print_red "You must run this script with the BW_SESSION set"
+    print_yellow "Check the status with:"
+    print_yellow "  bw status"
+    print_yellow "If it is unauthenticated, run:"
+    print_yellow '  export BW_SESSION=$(bw login [BW_EMAIL] --raw)'
+    print_yellow "If it is unlocked, run:"
+    print_yellow '  export BW_SESSION=$(bw unlock --raw)'
+    print_yellow "You can also set the Bitwarden email:"
+    print_yellow '  export BW_EMAIL="your@email.com"'
+    print_yellow "And run the script again. (It will ask for the master password)"
+    print_red_frame
     exit 
 fi
 
+# Esegui lo script Chezmoi
+print_green_frame
+print_green "Running Chezmoi Script..."
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply karimone
+print_green_frame
+
+print_green "All done!"
+
--- run_once_99-last-info.sh
+++ run_once_99-last-info.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
-#
 
-echo 'You can set the shell using chsh -s $(whereis zsh)'
-echo 'Remember to logout and login! (or restart)'
+echo ""
+tput setaf 4; echo "+-------------------------------------------------------+"
+tput setaf 4; echo "|                                                       |"
+tput setaf 4; echo "|  $(tput setaf 2)You can set the shell using $(tput setaf 3)chsh -s [path-to-zsh]$(tput sgr0)    $(tput setaf 4)|"
+tput setaf 4; echo "|  $(tput setaf 2)Remember to logout and login! (or restart)$(tput setaf 4)           |"
+tput setaf 4; echo "|                                                       |"
+tput setaf 4; echo "+-------------------------------------------------------+"
+echo ""
+echo "   .-=========-."
+echo "   \\'-=======-'/"
+echo "   _|   .=.   |_"
+echo "  ((|  {{1}}  |))"
+echo "   '|         |'"
+echo "    \\|  ' '| /"
+echo "     | |\\ /| |"
+echo "     | |( )| |"
+echo "jgs  \\|-=-|/"
+echo "      |   |"
+echo "      |   |"
+echo "      |   |"
+echo "      |   |"
+echo "     /     \\"
+echo "    /       \\"
+echo "   /_________\\"
+echo "      |   |"
+echo "     _|   |_"
+echo "    (_______)"
+echo ""
+echo "All done!"
 
