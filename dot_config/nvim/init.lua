05f745242f99af3ecef2d51cb337ad940791e72d
Karim N. Gorjux <karimone@gmail.com>
2024-03-03 09:20:46 +1100

    Update docs init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,1 +1,6 @@
+-- All the configuration is in the user
+-- You need to install the following sofware:
+-- ripgrep: for telescope
+-- lua-language-server: for the Lua LSP
+-- pyright: for the Python LSP
 require("user")
