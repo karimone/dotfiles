731a9feab700836020060c6bd146ce94ad215e33
Karim N. Gorjux <karimone@gmail.com>
2024-03-12 23:07:55 +1100

    Update nvim and kitty config

--- dot_config/nvim/lua/user/keymaps.lua
+++ dot_config/nvim/lua/user/keymaps.lua
@@ -1,16 +1,18 @@
 -- <C>    Represents the control key
 -- <A>    Represents the alt key.
 -- <C-S>  Represents the control + shift key
-local set = vim.keymap.set
-
+--
 vim.g.mapleader = " "                   -- Space is the leader key
 
+local set = require("user.common").set
+local cmd = vim.cmd
+
 -- Actions
-set('n', '<C-q>', vim.cmd.quit)        -- Simple quit
+set('n', '<C-q>', cmd.quit, "Simple Quit")
 
 -- Windows
-set('n', '<leader>\\', vim.cmd.vs)      -- Vertical split
-set('n', '<leader>-', vim.cmd.split)    -- Horizontal split
+set('n', '<leader>\\', cmd.vs, "VSplit current window")
+set('n', '<leader>-', cmd.split, "HSplit current window")
 
 set('n', '<C-k>', '<C-w>k')             -- Move window above
 set('n', '<C-j>', '<C-w>j')             -- Move window below
@@ -18,36 +20,38 @@ set('n', '<C-h>', '<C-w>h')             -- Move window left
 set('n', '<C-l>', '<C-w>l')             -- Move window right
 
 -- Decrease current window height
-set('n', '<C-S-Up>', [[<cmd>horizontal resize +5<cr>]])
-set('n', '<C-S-Down>', [[<cmd>horizontal resize -5<cr>]])
-set('n', '<C-S-Left>', [[<cmd>vertical resize +5<cr>]])
-set('n', '<C-S-Right>', [[<cmd>vertical resize -5<cr>]])
-
+set('n', '<leader><Up>', [[<cmd>horizontal resize +5<cr>]])
+set('n', '<leader><Down>', [[<cmd>horizontal resize -5<cr>]])
+set('n', '<leader><Right>', [[<cmd>vertical resize +5<cr>]])
+set('n', '<leader><Left>', [[<cmd>vertical resize -5<cr>]])
 -- Tabs
-set('n', '<C-t>', vim.cmd.tabnew)         -- New tab
-set('n', '<C-w>', vim.cmd.tabclose)       -- Close tab
-set('n', '<C-p>', vim.cmd.tabprevious)    -- Previous tab
-set('n', '<C-n>', vim.cmd.tabnext)        -- Next tab
+set('n', '<A>t', cmd.tabnew, "Create new tab")
+set('n', '<A>q', cmd.tabclose, "Close tab")
+set('n', '<A-h>', cmd.tabprevious, "Previous Tab")
+set('n', '<A-l>', cmd.tabnext, "Next Tab")
 
 -- Buffers
-set('n', '<S-Left>', vim.cmd.bprevious)   -- Previous buffer
-set('n', '<S-Right>', vim.cmd.bnext)      -- Next buffer
-set('n', '<S-Del>', vim.cmd.bdel)         -- Delete buffer
-set('n', '<S-Down>', vim.cmd.buffers)     -- List of buffers
-set('n', '<S-Up>', ":buffer ")            -- Go to buffer
+set('n', '<A-j>', cmd.bprevious)     -- Previous buffer
+set('n', '<S-Tab>', cmd.bprevious)   -- Previous buffer
+set('n', '<Tab>', cmd.bnext)         -- Next buffer
+set('n', '<A-k>', cmd.bnext)         -- Next buffer
+set('n', '<A-Del>', cmd.bdel)        -- Delete buffer
+set('n', '<S-Down>', cmd.buffers)    -- List of buffers
+set('n', '<S-Up>', ":buffer ")       -- Go to buffer
 
 -- Utilities
--- set('i', '<Tab>', '<C-x><C-o>')           -- Omnicomplete
-set('i', '<C-c>', '<ESC>:wa<CR>')         -- Save all files (insert mode)
-set('n', '<C-c>', '<ESC>:wa<CR>')         -- Save all files (normal mode)
-set('n', '<leader>?', vim.cmd.map)        -- Show the keymaps help
-set('n', '<leader>f', ':find')            -- Find a file
-set('n', '<leader>e', ':Lex 18<CR>')      -- Show the explorer
-set('n', '<F3>', ':set list!<CR>')        -- Toggle list
-set('n', '<F4>', ':set hls!<CR>')         -- Toggle highlight search
+set('i', '<C-Space>', '<C-x><C-o>')           -- Omnicomplete
+set('i', '<C-c>', '<ESC>:wa<CR>', "Save all files")         -- Save all files (insert mode)
+set('n', '<C-c>', '<ESC>:wa<CR>', "Save all files")         -- Save all files (normal mode)
+set('n', '<leader>?', cmd.map)        -- Show the keymaps help
+-- set('n', '<leader>f', ':find')            -- Find a file
+set('n', '<leader>e', ':Neotree filesystem reveal left toggle<CR>', "Toggle Neotree")  -- Show the explorer
+-- set('n', '<leader>e', '<F2>')
+set('n', '<F3>', ':set list!<CR>', "Toggle list")        -- Toggle list
+set('n', '<F4>', ':set hls!<CR>', "Toggle highlight search")         -- Toggle highlight search
 
 local sub=':%s/<C-r><C-w>//g<Left><Left>'
-set('n', '<leader>r', sub);               -- Replace under the cursor
+set('n', '<leader>r', sub, "Replace current word");               -- Replace under the cursor
 
 
 -- Visual Mode
@@ -57,19 +61,19 @@ set('v', '<leader>d', '"_d')              -- Delete section to void register
 set('v', '<leader>p', '"_dP')             -- Delete section to void register and paste over it
 
 -- Movements
-set('n', 'J', 'mzJ`z')                    -- Join the lines keeping the cursor in place
-set('n', '<C-u>', '<C-u>M')               -- Scroll half page UP keeping the cursor in the middle
-set('n', '<C-d>', '<C-d>M')               -- Scroll half page DOWN keeping the cursor in the middle
-set('n', 'N', 'Nzzzv')                    -- Search backward keeping the cursor in the middle
-set('n', 'n', 'nzzzv')                    -- Search forward keeping the cursor in the middle
+set('n', 'J', 'mzJ`z', "Join lines keeping cursor in place")                    -- Join the lines keeping the cursor in place
+set('n', '<C-u>', '<C-u>M', "Scroll half page up")               -- Scroll half page UP keeping the cursor in the middle
+set('n', '<C-d>', '<C-d>M', "Scroll half page down")               -- Scroll half page DOWN keeping the cursor in the middle
+set('n', 'N', 'Nzzzv', "Search backward")                    -- Search backward keeping the cursor in the middle
+set('n', 'n', 'nzzzv', "Search forward")                    -- Search forward keeping the cursor in the middle
 
 
 -- Location List
-set('n', '<C-PageUP>', vim.cmd.lprev)     -- Previous in navigation list
-set('n', '<C-PageDOWN>', vim.cmd.lnext    -- Next in navigation list
+set('n', '<C-PageUP>', cmd.lprev)     -- Previous in navigation list
+set('n', '<C-PageDOWN>', cmd.lnext)   -- Next in navigation list
 
 -- My hacks
-set('n', 'yy', 'Vyo<ESC>P')             -- Duplicate the current line
+set('n', 'yy', 'Vyj^P')               -- Duplicate the current line
 
 -- Disable arrow keys!
 set('n', '<Up>', '<Nop>')
@@ -78,3 +82,17 @@ set('n', '<Left>', '<Nop>')
 set('n', '<Right>', '<Nop>')
 set('n', '<Down>', '<Nop>')
 set('n', '<PageDown>', '<Nop>')
+
+-- Reload and edit the VIM RC
+set("n", "<leader>ns", ":source $MYVIMRC<CR>")
+
+-- Terminal mode
+set("t", "<Esc>", "<C-\\><C-n>")    -- terminal mode esc = normal mode
+set("t", "<leader><Esc>", "<Esc>")     -- now map esc to ctrl+v esc
+
+-- Easier navigation for long lines
+set("n", "j", "gj")
+set("n", "k", "gk")
+
+
+set("n", "<leader>z", ":ZenMode<CR>")
--- dot_config/nvim/lua/user/options.lua
+++ dot_config/nvim/lua/user/options.lua
@@ -2,7 +2,9 @@ local opt = vim.opt
 local cwd = vim.fn.getcwd()
 local current_path = ""..cwd..", "..cwd.."/**"
 
-opt.shortmess = "I"		  -- Remove splashscreen
+opt.clipboard = "unnamedplus" -- use system clipboard
+
+-- opt.shortmess = "I"		  -- Remove splashscreen
 opt.number = true		    -- Show line numbers
 opt.cursorline = true		-- Show cursor line
 opt.wrap = false		    -- Do not wrap text
@@ -11,7 +13,7 @@ opt.wrap = false		    -- Do not wrap text
 -- show menu even with only one entry, do not select nor insert, show a preview
 opt.completeopt = "menuone,noselect,noinsert,preview"
 opt.complete = "."		-- Only use current buffer for autocomplete
-opt.pumheight = 10		-- Height of the pop up window
+opt.pumheight = 15		-- Height of the pop up window
 
 -- Indentation
 opt.smartindent = true		-- Check the code to determine the auto indent
@@ -25,5 +27,25 @@ opt.splitright = true     -- Split the window on the right of the current one
 opt.hlsearch = true       -- Highlights the search
 opt.incsearch = true      -- Update search while typing
 opt.exrc = true           -- Enable .nvim.lua configuration on the folder
-opt.mouse = ""            -- Disable mouse
+opt.mouse = "a"           -- Enable mouse
 opt.path = current_path   -- Path to the current working dir
+opt.scrolloff = 999      -- Keep the cursor in the middle of the screen
+
+vim.opt.colorcolumn = "119" -- Show a column at 119 characters
+vim.opt.encoding = "utf-8"  -- Set encoding to utf-8
+
+-- Show special characters
+vim.opt.listchars = [[tab:\|_,nbsp:␣,trail:•,extends:⟩,precedes:⟨]]
+
+-- soft wrap the text at 120 characters
+-- vim.opt.textwidth = 120
+vim.opt.wrap = true
+
+vim.opt.relativenumber = true  -- Show relative line numbers
+
+-- set the folding based on the syntax
+vim.opt.foldmethod = "syntax"
+
+-- set the session options
+opt.sessionoptions = "buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
+
